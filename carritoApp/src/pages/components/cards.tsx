e98b6b86b74dc3f529dce3e2c5b4630f8300d090
ariellezcano <ariellezcano1995@gmail.com>
2023-06-05 08:42:31 -0300

    ...

--- carritoApp/src/pages/components/cards.tsx
+++ carritoApp/src/pages/components/cards.tsx
@@ -1,10 +1,25 @@
-function Cards(){
+import "./cards.css";
 
-    return(
-        <div className="Cards">
-            
+function Cards({ children }) {
+  return (
+    <div className="contenedor">
+      {children?.map((elemento: any, index: number) => (
+        <div className="col" key={index}>
+          <p>
+            {" "}
+            <b> {elemento.nombre} </b>
+          </p>
+          <p>
+            {" "}
+            <b>{elemento.descripcion} </b>
+          </p>
+          <p>
+            <b>${elemento.precio}</b>
+          </p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
--- carritoApp/src/pages/lst/LstProducto.tsx
+++ carritoApp/src/pages/lst/LstProducto.tsx
@@ -1,18 +1,14 @@
+import Cards from "../components/cards";
 import "./lst-producto.css";
 
 function LstProducto(props) {
-
   const { children } = props;
   return (
-    <div className="contenedor">
+    <div className="Lst">
       <h1>Listado de Productos</h1>
-      {children?.map((elemento: any, index: number) => (
-        <div className="col" key={index}>
-          <p>{elemento.nombre}</p>
-          <p>{elemento.descripcion}</p>
-          <p>{elemento.precio}</p>
-        </div>
-      ))}
+      <div className="contenedor">
+          <Cards children={children} />
+      </div>
     </div>
   );
 }
